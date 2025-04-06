5504fd9a691fbe7fd7e8618aba1848899c2773c0
Viktor Dmitriyev <2291074+vdmitriyev@users.noreply.github.com>
2025-04-06 20:56:04 +0200

    EDIT: increase version of CLI

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ var defaultGroupByForLogDate string = "month"
 
 const REPOSITORIES_DIRECTORY = ".repositories"
 
-var version string = "0.1.1"
+var version string = "0.1.2"
 var build string = "0.0.0" // do not remove or modify
 
 type UserContribution struct {
